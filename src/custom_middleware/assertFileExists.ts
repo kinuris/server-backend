a7e4757f6d9276b70c32fb639726e186e79de952
Chris Dominic Chan <chanchrisdominic@gmail.com>
2022-08-28 20:44:12 +0800

    added name

--- src/custom_middleware/assertFileExists.ts
+++ src/custom_middleware/assertFileExists.ts
@@ -3,10 +3,11 @@ import path from "path"
 import fs from "fs"
 
 export function assertFileExists(req: Request, res: Response, next: NextFunction): void {
+    const name = req.params["name"]
     const directory = req.params["directory"]
     const file = req.params["file"]
 
-    if (!fs.existsSync(path.join(__dirname, "sites", directory, file))) {
+    if (!fs.existsSync(path.join(__dirname, "sites", name, directory, file))) {
         res.status(404)
         res.sendFile(path.join(__dirname, "sites", "doesnotexist.html"))
         return
