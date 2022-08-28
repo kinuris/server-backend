f13f509c393761732c9fbf683de0981b839adfff
Chris Dominic Chan <chanchrisdominic@gmail.com>
2022-08-28 20:34:39 +0800

    added specific prod and dev build commands

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,29 @@
 const path = require("path")
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
 
-module.exports = {
-    entry: "./src/index.ts",
-    output: {
-        path: __dirname,
-        filename: "index.bundle.js"
-    },
-    mode: "production",
-    target: "node",
-    module: {
-        rules: [
-            {
-                test: /\.ts/,
-                loader: "ts-loader"
-            }
-        ]
-    },
-    resolve: {
-        extensions: [".ts", ".js", ".json"]
-    },
-    plugins: [new NodePolyfillPlugin()]
+module.exports = (env) => {
+
+    console.log(env)
+
+    return {
+        entry: "./src/index.ts",
+        output: {
+            path: __dirname,
+            filename: "index.bundle.js"
+        },
+        mode: env.prod ? "production" : "development" ,
+        target: "node",
+        module: {
+            rules: [
+                {
+                    test: /\.ts/,
+                    loader: "ts-loader"
+                }
+            ]
+        },
+        resolve: {
+            extensions: [".ts", ".js", ".json"]
+        },
+        plugins: [new NodePolyfillPlugin()]
+    }
 }
