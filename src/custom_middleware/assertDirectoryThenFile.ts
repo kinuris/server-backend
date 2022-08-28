42b6a371537b143c82eda541cf700aea32b16560
Chris Dominic Chan <chanchrisdominic@gmail.com>
2022-08-28 20:20:22 +0800

    removed res.end call

--- src/custom_middleware/assertDirectoryThenFile.ts
+++ src/custom_middleware/assertDirectoryThenFile.ts
@@ -4,7 +4,6 @@ import path from "path"
 export function assertDirectoryThenFile(req: Request, res: Response, next: NextFunction): void {
     if (req.params["directory"] && !req.params["file"]) {
         res.status(403).sendFile(path.join(__dirname, "sites", "forbidden.html"))
-        res.end()
         return
     }
 
