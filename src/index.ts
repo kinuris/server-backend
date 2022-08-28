7eb23269e615e2f05115147dd40b250201843698
Chris Dominic Chan <chanchrisdominic@gmail.com>
2022-08-28 19:38:08 +0800

    refactored to integrate middleware

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express from "express"
 import path from "path"
+import { assertDirectoryThenFile } from "./custom_middleware/assertDirectoryThenFile"
+import { nameOnlyRedirect } from "./custom_middleware/nameOnlyRedirect"
 
 const app = express()
 
@@ -7,6 +9,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "sites", "index", "index.html"))
 })
 
+app.get('/:name/:directory?/:file?', nameOnlyRedirect, assertDirectoryThenFile, (req, res) => {
+    res.sendFile(path.join(__dirname, "sites", req.url))
+})
+
 app.listen(5500, () => {
-    console.log("Server is listenening of port 5500")
+    console.log("Server is listening of port 5500")
 })
