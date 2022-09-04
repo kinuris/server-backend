da3720db80cbec2b7b51e57275114b94857d60f6
Chris Dominic Chan <chanchrisdominic@gmail.com>
2022-09-04 14:54:35 +0800

    reverted callbacks

--- src/custom_middleware/nameOnlyRedirect.ts
+++ src/custom_middleware/nameOnlyRedirect.ts
@@ -3,9 +3,7 @@ import path from "path"
  
 export function nameOnlyRedirect(req: Request, res: Response, next: NextFunction): void {
     if (!req.params["directory_or_file"] && !req.params["file"]) {
-        res.sendFile(path.join(__dirname, "sites", req.params["name"], "index.html"), () => {
-            
-        })
+        res.sendFile(path.join(__dirname, "sites", req.params["name"], "index.html"))
 
         return
     }
