8fcd24ac0f0f2c66e2f238006ee3531abcd6a2b4
Chris Dominic Chan <chanchrisdominic@gmail.com>
2022-09-21 19:49:03 +0800

    added CRD operations webpage

---
+++ sites/register-item/controller.js
@@ -0,0 +1,43 @@
+const siteUrl = "https://idontknowanymore.xyz"
+
+const form = document.getElementById("register-form")
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+    
+    const name = event.target.name.value
+    const price = parseFloat(event.target.price.value)
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", siteUrl + "/create-item")
+    xhr.setRequestHeader("Content-Type", "application/json")
+    xhr.send(JSON.stringify({
+        name,
+        price
+    }));
+})
+
+const deleteForm = document.getElementById("delete-form")
+
+deleteForm.addEventListener('submit', event => {
+    event.preventDefault()
+    
+    const name = event.target.name.value
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", siteUrl + "/delete-item")
+    xhr.setRequestHeader("Content-Type", "application/json")
+    xhr.send(JSON.stringify({
+        name
+    }));
+})
+
+const data = document.getElementById("data")
+
+setTimeout(async () => {
+    const res = await fetch(siteUrl + "/fetch")
+    const result = await res.json()
+
+    data.innerHTML = JSON.stringify(result.data)
+
+}, 1000)
