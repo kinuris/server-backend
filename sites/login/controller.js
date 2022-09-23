cfeb04eae64259ae2abc63f05ee3283be18ec318
Chris Dominic Chan <chanchrisdominic@gmail.com>
2022-09-23 17:09:11 +0800

    added sample login page

---
+++ sites/login/controller.js
@@ -0,0 +1,19 @@
+// const siteUrl = "https://idontknowanymore.xyz"
+const siteUrl = "http://localhost:5500"
+
+const form = document.getElementById("login-user")
+
+form.addEventListener("submit", event => {
+    event.preventDefault()
+
+    const email = event.target.email.value
+    const password = event.target.password.value
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", siteUrl + "/login")
+    xhr.setRequestHeader("Content-Type", "application/json")
+    xhr.send(JSON.stringify({
+        email,
+        password
+    }))
+})
