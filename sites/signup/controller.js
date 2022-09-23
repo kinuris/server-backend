d277614de56b6d8abb9eeb696674daafe4b55666
Chris Dominic Chan <chanchrisdominic@gmail.com>
2022-09-23 17:09:20 +0800

    added sample signup page

---
+++ sites/signup/controller.js
@@ -0,0 +1,19 @@
+const siteUrl = "http://localhost:5500"
+const form = document.getElementById("create-user")
+
+form.addEventListener("submit", event => {
+    event.preventDefault()
+
+    const username = event.target.username.value
+    const email = event.target.email.value
+    const password = event.target.password.value
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", siteUrl + "/signup")
+    xhr.setRequestHeader("Content-Type", "application/json")
+    xhr.send(JSON.stringify({
+        username,
+        email,
+        password
+    }))
+})
