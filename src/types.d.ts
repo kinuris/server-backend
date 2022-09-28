f76a787a5ae2200c532c2eebab7c7cc252d1b7b4
Chris Dominic Chan <chanchrisdominic@gmail.com>
2022-09-28 20:04:27 +0800

    added RouterOption

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,3 +2,10 @@ type RedirectOption = {
     alertMessage?: string,
     expireMessage?: string
 }
+
+type RouterOption = {
+    [key: string]: {
+        redirect: string,
+        onFail: boolean
+    }
+}
