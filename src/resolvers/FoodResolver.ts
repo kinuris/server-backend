3c1069c466a09428755a962d8369377b0ce71227
Chris Dominic Chan <chanchrisdominic@gmail.com>
2022-09-29 15:59:44 +0800

    added resolver

---
+++ src/resolvers/FoodResolver.ts
@@ -0,0 +1,10 @@
+import { Resolver, Query } from "type-graphql"
+import { Food } from "../entity/Food";
+
+@Resolver(Food)
+export class FoodResolver {
+    @Query(returns => [Food])
+    async fetchAll() {
+        return await Food.find()
+    }
+}
