c7a43ccff87fa13362d8466c9ac58f26fff1fe5b
Chris Dominic Chan <chanchrisdominic@gmail.com>
2022-10-05 21:20:07 +0800

    added admin check to registerFoodAndVariants

--- src/resolvers/FoodVariantsResolver.ts
+++ src/resolvers/FoodVariantsResolver.ts
@@ -1,12 +1,34 @@
-import { Resolver, Query, Mutation, Arg } from "type-graphql"
+import { Context } from "apollo-server-core";
+import { Request, Response } from "express";
+import { Resolver, Query, Mutation, Arg, Ctx } from "type-graphql"
 import { Food } from "../entity/Food";
 import { FoodVariants } from "../entity/FoodVariants";
 import { RegisterFoodAndVariants } from "./inputs/composites/registerFoodAndVariants";
 
+import jwt from "jsonwebtoken"
+
 @Resolver(FoodVariants)
 export class FoodVariantsResolver {
-    @Mutation(returns => [FoodVariants])
-    async registerFoodAndVariants(@Arg('foodAndVariants') foodAndVariants: RegisterFoodAndVariants) {
+    @Mutation(returns => [FoodVariants], { nullable: true })
+    async registerFoodAndVariants(@Ctx() context: Context<{ req: Request, res: Response }>, @Arg('foodAndVariants') foodAndVariants: RegisterFoodAndVariants) {
+        const { req } = context
+
+        if(req.body["auth_token"]) {
+            try {
+                jwt.verify(req.body["auth_token"], process.env.SECRET)
+            } catch (error) {
+                return null
+            }
+        } else {
+            return null
+        }
+
+        const decodedPayload = jwt.decode(req.body["auth_token"]) as jwt.JwtPayload
+
+        if(!decodedPayload["admin"]){
+            return null
+        }
+        
         const { food, variants } = foodAndVariants
         const { name, imageLink } = food
         
