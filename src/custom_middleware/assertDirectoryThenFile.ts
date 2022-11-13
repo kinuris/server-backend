98003bec2e353e8cc2f5d6ba362d5139e492985a
Chris Dominic Chan <chanchrisdominic@gmail.com>
2022-11-13 13:39:35 +0800

    added static routing check

--- src/custom_middleware/assertDirectoryThenFile.ts
+++ src/custom_middleware/assertDirectoryThenFile.ts
@@ -6,11 +6,13 @@ export function assertDirectoryThenFile(req: Request, res: Response, next: NextF
 
         if (req.params["directory_or_file"].split('.').length >= 2) {
             res.sendFile(path.join(__dirname, "sites", req.params["name"], req.params["directory_or_file"]))
-
             return
         } else {
             if (req.body["should_browser_route"]) {
                 res.sendFile(path.join(__dirname, "sites", req.params["name"], "index.html"))
+                return
+            } else if (req.body["should_static_route"]) {
+                res.sendFile(path.join(__dirname, "sites", req.params["name"], req.params["directory_or_file"], "index.html"))
                 return
             } else {
                 res.status(403).sendFile(path.join(__dirname, "sites", "default", "forbidden.html"))
